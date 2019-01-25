b66a41b8af01d7df8a22323ad4998cc74d1930e3
Yurii Yaremko <yurii.yaremko@teamvoy.com>
2019-01-25 18:20:00 +0200

    Add some template and styling to screens

--- src/presentation/screens/Main/index.js
+++ src/presentation/screens/Main/index.js
@@ -1,16 +1,59 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-
-export default class Main extends React.Component {
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+ class Main extends React.Component {
 
   static navigationOptions = {
     header:null
-}
+  }
+
+  renderItem = () => {
+    const arr = [
+      {
+        id: 1,
+        text: 'first text'
+      },
+       {
+         id: 2,
+         text: 'second text'
+       },
+       {
+        id: 3,
+        text: 'first text'
+      },
+      {
+        id: 4,
+        text: 'first text'
+      },
+      {
+        id: 5,
+        text: 'first text'
+      },
+      {
+        id: 6,
+        text: 'first text'
+      },
+    ];
+    
+    return(
+      arr.map(element => (
+        <View key={element.id} style={{ height: 80, flexDirection: 'row', paddingHorizontal: 20, borderBottomWidth: 1, borderColor: 'lightgrey', alignItems: 'center' }}>
+          <Text style={{ flex: 1, fontSize: 22 }}>
+            {element.text}
+          </Text>
+          <View style={{ backgroundColor: '#0f1f3d', height: 44, width: 44, borderRadius: 22, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{ color: 'white', fontSize: 20 }}>
+              22
+            </Text>
+          </View>
+        </View>
+      ))
+    );
+  }
 
   render() {
     return(
       <View>
-        <View style={{ backgroundColor: 'darkblue', height: 130, justifyContent: 'center', paddingLeft: 20 }}>
+        <View style={{ backgroundColor: '#0f1f3d', height: 130, justifyContent: 'center', paddingLeft: 20, paddingTop: 20 }}>
           <Text style={{ fontSize: 35, fontWeight: 'bold', color: 'white' }}>
             Sayer
           </Text>
@@ -18,27 +61,22 @@ export default class Main extends React.Component {
             World's most used time waster
           </Text>
         </View>
-        <View style={{ height: 80, flexDirection: 'row', paddingHorizontal: 15, borderBottomWidth: 1, borderColor: 'lightgrey', alignItems: 'center' }}>
-          <Text style={{ flex: 1, fontSize: 22 }}>
-            Some text to show
-          </Text>
-          <View style={{ backgroundColor: 'darkblue', height: 44, width: 44, borderRadius: 22, alignItems: 'center', justifyContent: 'center'}}>
-            <Text style={{ color: 'white', fontSize: 20 }}>
-              22
-            </Text>
-          </View>
-        </View>
-        <TouchableOpacity 
-          onPress={() => this.props.navigation.navigate('NewItem')}
-        >
-          <View style={{ marginTop: 25, height: 70, width: 70, borderRadius: 35, alignSelf: 'center', alignItems: 'center', justifyContent: 'center', backgroundColor: 'mediumvioletred'}}>
-            <Text style={{ fontSize: 50, fontWeight: 'bold', color: 'white'}}>
-              +
-            </Text>
-          </View>
-        </TouchableOpacity>
+        <ScrollView>
+          {this.renderItem()}
+          <TouchableOpacity 
+            onPress={() => this.props.navigation.navigate('NewItem')}
+            style={{ marginBottom: 160}}
+          >
+            <View style={{ marginTop: 25, height: 70, width: 70, borderRadius: 35, alignSelf: 'center', alignItems: 'center', justifyContent: 'center', backgroundColor: '#e6005c'}}>
+              <Text style={{ fontSize: 50, fontWeight: 'bold', color: 'white'}}>
+                +
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </ScrollView>
       </View>
-      
     );
   }
 }
+
+export default Main;
--- src/presentation/screens/NewItem/index.js
+++ src/presentation/screens/NewItem/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
 
 export default class NewItem extends React.Component {
 
@@ -10,10 +10,33 @@ export default class NewItem extends React.Component {
   render() {
     return(
       <View>
-        <View style={{ backgroundColor: 'darkblue', height: 140, justifyContent: 'center', paddingLeft: 20 }}>
-          <Text style={{ fontSize: 25, fontWeight: 'bold', color: 'white' }}>
+        <View 
+          style={{ backgroundColor: '#0f1f3d', height: 140, paddingLeft: 20,flexDirection: 'row', paddingTop: 20, alignItems: 'center'}}  
+        >
+          <TouchableOpacity 
+            style={{ backgroundColor: '#1f3d7a', height: 44, width: 44, borderRadius: 22, alignItems: 'center', justifyContent: 'center', marginRight: 20 }}
+            onPress={() => this.props.navigation.pop()}
+          >
+            <Text style={{ color: 'white', fontSize: 24 }}>
+              ←
+            </Text>
+          </TouchableOpacity>
+          <Text style={{ fontSize: 24, fontWeight: 'bold', color: 'white' }}>
             Create new item
           </Text>
+        </View>
+        <View style={{ flexDirection: 'row', paddingHorizontal: 20, paddingTop: 30 }}>
+          <TextInput 
+            placeholder="add new item"
+            style={{ flex: 1, marginRight: 15, borderBottomWidth: 1, borderColor: 'lightgrey', height: 40, fontSize: 20 }}
+            autoCorrect={false}
+            numberOfLines={1}
+          />
+          <TouchableOpacity style={{ backgroundColor: '#0f1f3d', height: 44, width: 44, borderRadius: 22, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{ color: 'white', fontSize: 24 }}>
+              >
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
