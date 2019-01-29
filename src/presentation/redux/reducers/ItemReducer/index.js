b6288a89df87b6f098092dab9f2fa8e1a025ce29
Yurii Yaremko <yurii.yaremko@teamvoy.com>
2019-01-29 13:54:27 +0200

    Remove hardcoded values

--- src/presentation/redux/reducers/ItemReducer/index.js
+++ src/presentation/redux/reducers/ItemReducer/index.js
@@ -1,38 +1,7 @@
 import { combineReducers } from 'redux';
 
 const INITIAL_STATE = {
-  text: [
-    {
-      id: 0,
-      value: 'hazard',
-      comments: ['best chelsea playerbest chelsea playerbest chelsea playerbest chelsea playerbest chelsea playerbest chelsea playerbest chelsea playerbest chelsea playerbest chelsea playerbest chelsea playerbest chelsea playerbest chelsea player dsadsadasda','n1','no to real']
-    },
-     {
-       id: 1,
-       value: 'second text',
-       comments: ['hello','dude']
-     },
-     {
-      id: 2,
-      value: 'first text',
-      comments: []
-    },
-    {
-      id: 3,
-      value: 'super long title to test how it is displayed on the screen ',
-      comments: []
-    },
-    {
-      id: 4,
-      value: 'first text',
-      comments: []
-    },
-    {
-      id: 5,
-      value: 'first text',
-      comments: []
-    },
-  ]
+  text: []
 }
 
 const itemReducer = (state = INITIAL_STATE, action) => {
